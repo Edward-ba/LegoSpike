ec0aa06aaaffc73945ed64aa7e0665168d76375d
Ed <ebalasingham@gmail.com>
2020-08-23 19:17:17 -0700

    [turn] it works now

--- turn.py
+++ turn.py
@@ -11,15 +11,15 @@ def turn(hub, drive_pair, angle_to_turn):
     angle_to_turn = angle_to_turn % 360
 
     # if (angle_to_turn > 0 and angle_to_turn < 150) or (angle_to_turn < 0 and angle_to_turn > -150):
-    if (angle_to_turn):
+    if angle_to_turn:
         print('AngleToTurn', angle_to_turn)
-        fudge = 0.7;
+        fudge = 0.7
         rotate_const = 586.0 / 360.0
 
         hub.motion_sensor.reset_yaw_angle()
-        turn = rotate_const * angle_to_turn
+        turn_angle = rotate_const * angle_to_turn
 
-        drive_pair.move(turn, 'degrees', steering=100, speed=30)
+        drive_pair.move(turn_angle, 'degrees', steering=100, speed=30)
         yaw_angle = hub.motion_sensor.get_yaw_angle()
 
         if yaw_angle < 0 and angle_to_turn > 0:
@@ -28,7 +28,7 @@ def turn(hub, drive_pair, angle_to_turn):
             yaw_angle -= 360
 
         print('Angle:', yaw_angle)
-        error_angle = yaw_angle - angle_to_turn;
+        error_angle = yaw_angle - angle_to_turn
         if error_angle >= 3:
             print('Error:', error_angle)
             drive_pair.move(-error_angle * rotate_const * fudge, 'degrees', steering=100, speed=10)
