4a5c10c77e36d29eafb76b9efe68c7230ffbbc66
Edward-ba <62915397+Edward-ba@users.noreply.github.com>
2020-09-24 17:40:54 -0700

    Update M06_and_M07.py

--- M06_and_M07.py
+++ M06_and_M07.py
@@ -6,8 +6,8 @@ import math
 hub = PrimeHub()
 motor_pair = MotorPair('A', 'E')
 wheel_distance_apart = 14.5
-wheel_diameter = 4.25
-wheel_circumference = 2 * math.pi * wheel_diameter
+wheel_radius = 4.25
+wheel_circumference = 2 * math.pi * wheel_radius
 motor_pair.set_motor_rotation(wheel_circumference, 'cm')
 
 motor_pair.move_tank("""the amount to be in line with M06""", 'cm', left_speed=25, right_speed=25)
