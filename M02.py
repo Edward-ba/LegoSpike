3c5d3d1c3d3639125e9501efcb50e75f320a7a81
Edward-ba <62915397+Edward-ba@users.noreply.github.com>
2020-09-24 17:39:46 -0700

    Update M02.py

--- M02.py
+++ M02.py
@@ -6,8 +6,8 @@ import math
 g_hub = PrimeHub()
 g_motor_pair = MotorPair('A', 'E')
 g_wheel_distance_apart = 14.5
-g_wheel_diameter = 4.25
-g_wheel_circumference = 2 * math.pi * g_wheel_diameter
+g_wheel_radius = 4.25
+g_wheel_circumference = 2 * math.pi * g_wheel_radius
 g_motor_pair.set_motor_rotation(g_wheel_circumference, 'cm')
 
 
