bec2d7fd16a03dd099ea4a298957df3569f98f3c
Ed <ebalasingham@gmail.com>
2020-10-03 20:41:55 -0700

    [line following] made it a bit neater

--- line_following.py
+++ line_following.py
@@ -3,30 +3,25 @@ from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, Motio
 from spike.control import wait_for_seconds, wait_until, Timer
 import math
 
-hub = PrimeHub()
-color = ColorSensor('D')
-left = Motor('A')
-right = Motor('E')
-door_bell = ForceSensor('B')
-motor_pair = MotorPair('A', 'E')
-wheel_distance_apart = 14.5
-wheel_radius = 4.25
-wheel_circumference = 2 * math.pi * wheel_radius
-motor_pair.set_motor_rotation(wheel_circumference, 'cm')
-motor_pair.set_default_speed(-50)
-count = 0
+g_hub = PrimeHub()
+g_right_sensor = ColorSensor('D')
+g_motor_pair = MotorPair('A', 'E')
+g_wheel_distance_apart = 14.5
+g_wheel_radius = 4.25
+g_wheel_circumference = 2 * math.pi * g_wheel_radius
+g_motor_pair.set_motor_rotation(g_wheel_circumference, 'cm')
+g_motor_pair.set_default_speed(-50)
+g_motor_pair.set_stop_action('break')
 
-while count < 200:
-    if door_bell.is_pressed():
-        left.stop()
-        right.stop()
-        break
 
-    if color.get_reflected_light() > 50:
-        left.start_at_power(-25)
-        right.start_at_power(15)
-    else:
-        right.start_at_power(20)
-        left.start_at_power(15)
+def line_follow(hub, motor_pair, right_sensor, num):
+    count = 0
+    while count < num:
+        if right_sensor.get_reflected_light() > 50:
+            motor_pair.start_tank(-25, 15)
+        else:
+            motor_pair.start_tank(20, 15)
+        count += 1
 
-motor_pair.move(25.4, 'cm')
+
+line_follow(g_hub, g_motor_pair, g_right_sensor, 200)
